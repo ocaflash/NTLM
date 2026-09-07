a6110e362c34e59bee8c2a39213100fd049eee46
Aleksandr Ostapchuk <87189132+ocaflash@users.noreply.github.com>
2026-09-07 17:28:02 +0300

    Update module.sh

--- module.sh
+++ module.sh
@@ -1,2 +1,4 @@
-grep -RhsE '^(deb-src|Types:.*deb-src)' /etc/apt/sources.list /etc/apt/sources.list.d 2>/dev/null
+echo 'deb-src http://deb.debian.org/debian trixie main' | sudo tee -a /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian trixie-updates main' | sudo tee -a /etc/apt/sources.list
+echo 'deb-src http://deb.debian.org/debian-security trixie-security main' | sudo tee -a /etc/apt/sources.list
 sudo apt-cache showsrc libnginx-mod-http-auth-spnego | grep -E '^(Package|Version|Build-Depends):'
